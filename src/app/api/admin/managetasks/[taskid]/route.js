5ca19271d09e684d7f88bf957ad86a32b42d9992
Tokom Nyori <tokom.nyori@gmail.com>
2023-10-31 11:41:14 +0530

    edited

--- src/app/api/admin/managetasks/[taskid]/route.js
+++ src/app/api/admin/managetasks/[taskid]/route.js
@@ -1,6 +1,6 @@
 import { connectDB } from "@/helper/db"
 import { getResponseMsg } from "@/helper/getResponseMsg"
-import { Task } from "@/models/notemodel"
+import { NoteModel } from "@/models/notemodel"
 
 connectDB()
 
@@ -8,7 +8,7 @@ connectDB()
 export async function GET(request, { params }) {
     const { taskid } = params
     try {
-        const task = await Task.findById({ _id: taskid })
+        const task = await NoteModel.findById({ _id: taskid })
         return getResponseMsg(
             { message: 'Task fetched successfully', status: 200, success: true, body: task }
         )
