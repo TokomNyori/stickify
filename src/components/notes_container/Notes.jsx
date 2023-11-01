b0dd6847b7cc7e4da483d03ceed6f7b1efe4feeb
Tokom Nyori <tokom.nyori@gmail.com>
2023-11-02 00:22:56 +0530

    testing noteContainer11

--- src/components/notes_container/Notes.jsx
+++ src/components/notes_container/Notes.jsx
@@ -40,18 +40,18 @@ const Notes = ({ notes, deleteNotes, deletedNotes, noteType, togglePinned }) => 
                             ${deletedNotes[note._id] ? 'shrink' : ''} cursor-pointer shadow-md dark:brightness-[85%]`} key={note._id}
                 onClick={(e) => toTheNotePage(e, note._id)}
             >
-                <div className='truncate text-[1rem] font-bold'>
+                <div className='truncate text-[1rem] sm:text-sm font-bold'>
                     {note.title}
                 </div>
-                <div className='line-clamp-6 text-[0.9rem] mt-2 flex-grow' style={{ whiteSpace: 'pre-line' }}>
+                <div className='line-clamp-6 text-[0.9rem] sm:text-[0.8rem] mt-2 flex-grow' style={{ whiteSpace: 'pre-line' }}>
                     {note.content}
                 </div>
-                <div className='text-sm mt-4 flex justify-between items-center gap-2'>
+                <div className='text-sm sm:mt-4 mt-5 flex justify-between items-center gap-2'>
                     {
                         note.isPrivate ?
-                            <div className='flex justify-start items-center gap-1 text-xs'>
+                            <div className='flex justify-start items-center gap-1 sm:text-xs text-sm'>
                                 Private
-                            </div> 
+                            </div>
                             :
                             <div className='flex justify-start items-center gap-1 text-xs'>
                                 {note.likes < 1 ? <span></span> :
@@ -65,29 +65,24 @@ const Notes = ({ notes, deleteNotes, deletedNotes, noteType, togglePinned }) => 
                                 <div onClick={(e) => togglePinned(e, note._id, 'remove')} >
                                     <BsPinAngleFill
                                         className='text-gray-700 transition ease-in-out duration-300 hover:scale-125 
-                                            active:text-black' />
+                                            active:text-black text-lg sm:text-sm' />
                                 </div> :
                                 <div onClick={(e) => togglePinned(e, note._id, 'add')}>
                                     <BsPinAngle
                                         className='text-gray-600/70 transition ease-in-out duration-300 hover:scale-125 
-                                            active:text-black' />
+                                            active:text-black text-lg sm:text-sm' />
                                 </div>
                         }
                         <div onClick={(e) => deleteNotes(e, note._id)}>
                             <MdDeleteOutline
                                 className=' text-gray-700/70 font-light transition ease-in-out duration-300 hover:scale-125 
-                                    active:text-black' />
+                                    active:text-black text-lg sm:text-sm' />
                         </div>
                         <div onClick={(e) => editNote(e, note._id)}>
                             <MdOutlineModeEditOutline
                                 className='text-gray-600/70 font-light transition ease-in-out duration-300 hover:scale-125 
-                                    active:text-black' />
+                                    active:text-black text-lg sm:text-sm' />
                         </div>
-                        {/* <div onClick={(e) => toTheNotePage(e, note._id)}>
-                                <HiMiniViewfinderCircle
-                                    className='text-gray-600/70 transition ease-in-out duration-300 hover:scale-125 
-                                    active:text-black' />
-                            </div> */}
                     </div>
                 </div>
             </div>
