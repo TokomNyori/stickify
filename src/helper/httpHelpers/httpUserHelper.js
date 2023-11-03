7fa47764a04cdd8234038b2137e996fe44b1aec3
Tokom Nyori <tokom.nyori@gmail.com>
2023-11-03 23:36:00 +0530

    testing7

--- src/helper/httpHelpers/httpUserHelper.js
+++ src/helper/httpHelpers/httpUserHelper.js
@@ -82,7 +82,7 @@ export const deleteSelfHelper = async ({ method, headers, body, id }) => {
 export const getGlobalUsersHelper = async ({ method }) => {
     const res = await fetch(`api/admin/manageusers?timestamp=${Date.now()}`, {
         method: method,
-        cache: 'no-store',
+        headers: { cache: 'no-store' },
     })
     if (!res.ok) {
         const errorData = await res.json();
