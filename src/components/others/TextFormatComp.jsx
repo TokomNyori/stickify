23aaeb17d730f7384ad269cc5f8c067d67e703fa
Tokom Nyori <tokom.nyori@gmail.com>
2023-12-01 02:14:48 +0530

    Big Update enhancement: 20

--- src/components/others/TextFormatComp.jsx
+++ src/components/others/TextFormatComp.jsx
@@ -25,43 +25,43 @@ const TextFormatComp = ({ applyFormattingToSelectedText, parentRef }) => {
             className={`${'flex gap-2 bg-transparent'} absolute top-0`}>
             <div
                 className='font-extrabold border border-gray-800 rounded-md px-1 py-0 cursor-pointer w-6 text-center
-                backdrop-blur-[6px]'
+                backdrop-blur-[7px]'
                 onClick={() => applyFormattingToSelectedText('bold')}>
                 B
             </div>
             <div
                 className={`${caveat.className} italic border border-gray-800 rounded-md px-1 py-0 cursor-pointer 
-            w-6 text-center font-normal backdrop-blur-[6px]`}
+            w-6 text-center font-normal backdrop-blur-[7px]`}
                 onClick={() => applyFormattingToSelectedText('italic')}>
                 i
             </div>
             <div
                 className=' underline border border-gray-800 rounded-md px-1 py-0 cursor-pointer w-6 text-center
-            backdrop-blur-[6px]'
+            backdrop-blur-[7px]'
                 onClick={() => applyFormattingToSelectedText('underline')}>
                 U
             </div>
             <div className=' flex justify-center items-center border border-gray-800 rounded-md px-1 py-0 cursor-pointer 
-            w-7 text-center backdrop-blur-[6px]'
+            w-7 text-center backdrop-blur-[7px]'
                 onClick={() => applyFormattingToSelectedText('bullet')}>
                 <MdOutlineFormatListBulleted className='text-[1rem]' />
             </div>
             <div className=' border border-gray-800 rounded-md px-1 py-0 cursor-pointer w-7 text-center
-            backdrop-blur-[6px]'
+            backdrop-blur-[7px]'
                 onClick={() => applyFormattingToSelectedText('h1')}>
                 h1
             </div>
             <div className=' border border-gray-800 rounded-md px-1 py-0 cursor-pointer w-7 text-center
-            backdrop-blur-[6px]'
+            backdrop-blur-[7px]'
                 onClick={() => applyFormattingToSelectedText('h2')}>
                 h2
             </div>
             <div className=' border border-gray-800 rounded-md px-1 py-0 cursor-pointer w-7 text-center
-            backdrop-blur-[6px]'
+            backdrop-blur-[7px]'
                 onClick={() => applyFormattingToSelectedText('h3')}>
                 h3
             </div>
-            <div className='rounded-full px-1 py-0 cursor-move text-gray-800 backdrop-blur-[6px]
+            <div className='rounded-full px-1 py-0 cursor-move text-gray-800 backdrop-blur-[7px]
             text-center'
             >
                 <IoMove className='text-[1.4rem] -ml-2' />
