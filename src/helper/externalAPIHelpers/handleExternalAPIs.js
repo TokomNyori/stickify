60a4fdeeeb8a73bd70e773d35f378585784d8c23
Tokom Nyori <tokom.nyori@gmail.com>
2023-12-06 00:33:11 +0530

    Big Update enhancement: 31

--- src/helper/externalAPIHelpers/handleExternalAPIs.js
+++ src/helper/externalAPIHelpers/handleExternalAPIs.js
@@ -72,16 +72,16 @@ export async function youtubeTenVideotHelper({ method, title, headers }) {
 //     }
 // }
 
-export const handleTextToSpeech = async ({ text }) => {
-    const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY });
-    const speechFile = path.resolve("./speech.mp3");
+// export const handleTextToSpeech = async ({ text }) => {
+//     const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY });
+//     const speechFile = path.resolve("./speech.mp3");
 
-    const mp3 = await openai.audio.speech.create({
-        model: "tts-1",
-        voice: "onyx",
-        input: `${'Hello, this is Tokom. I am a software developer!!! Yes! Yes I am! And I feel so good about it!'}`,
-    });
-    console.log(speechFile);
-    const buffer = Buffer.from(await mp3.arrayBuffer());
-    await fs.promises.writeFile(speechFile, buffer);
-}
+//     const mp3 = await openai.audio.speech.create({
+//         model: "tts-1",
+//         voice: "onyx",
+//         input: `${'Hello, this is Tokom. I am a software developer!!! Yes! Yes I am! And I feel so good about it!'}`,
+//     });
+//     console.log(speechFile);
+//     const buffer = Buffer.from(await mp3.arrayBuffer());
+//     await fs.promises.writeFile(speechFile, buffer);
+// }
