e0ca974800444699dbf3b3df2863fc3e7b7a70d0
Tokom Nyori <tokom.nyori@gmail.com>
2023-12-06 01:15:10 +0530

    Big Update enhancement: 34

--- src/components/modals/NoteModal.jsx
+++ src/components/modals/NoteModal.jsx
@@ -1155,7 +1155,7 @@ const NoteModal = () => {
                             data-testid="loader"
                             speedMultiplier={1}
                         />
-                        <div className="text-2xl mt-5 font-bold text-[#1F2937]">
+                        <div className="text-2xl mt-5 font-bold backdrop-blur-[5px] p-1 text-[#1F2937]">
                             Getting videos...
                         </div>
                     </div>
@@ -1173,7 +1173,7 @@ const NoteModal = () => {
                             data-testid="loader"
                             speedMultiplier={1}
                         />
-                        <div className="text-2xl mt-5 font-bold text-[#1F2937]">
+                        <div className="text-2xl mt-5 backdrop-blur-[5px] font-bold p-1 text-[#1F2937]">
                             Gathering thoughts... 🍂
                         </div>
                         {/* <div className="text-lg mt-2 font-bold text-[#1F2937] text-center">
