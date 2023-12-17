2592cf110b3bd2d0ee432c68e8b9e7793a6ed819
Tokom Nyori <tokom.nyori@gmail.com>
2023-12-17 23:13:48 +0530

    Big Inclusion: 31

--- src/app/notes/[note]/page.jsx
+++ src/app/notes/[note]/page.jsx
@@ -2,7 +2,7 @@ import NotePage from "@/components/notes_container/NotePage"
 
 const Note = ({ params }) => {
     return (
-        <main className="sm:px-52">
+        <main className="sm:px-16 lg:px-52">
             <NotePage params={params} />
         </main>
     )
